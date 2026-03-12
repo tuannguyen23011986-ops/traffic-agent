b6018cc590f003de7e7a41c22f736249d490c0a3
tuannguyen23011986-ops <tuannguyen23011986@gmail.com>
2026-03-12 14:43:14 +0700

    Refactor environment variable usage and cleanup code

--- agent.py
+++ agent.py
@@ -3,38 +3,38 @@ import datetime
 import urllib.parse
 import os
 
-GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
-ZALO_ACCESS_TOKEN = os.environ["ZALO_TOKEN"]
-ZALO_USER_ID = os.environ["ZALO_USER_ID"]
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+ZALO_ACCESS_TOKEN = os.getenv("ZALO_TOKEN")
+ZALO_USER_ID = os.getenv("ZALO_USER_ID")
 
-ORIGIN = "Trường Tiểu học Võ Thị Sáu Quận 7 TP HCM"
-DESTINATION = "285 Cách Mạng Tháng 8 Quận 10 TP HCM"
+origin = "Trường Tiểu học Võ Thị Sáu Quận 7 TP HCM"
+destination = "285 Cách Mạng Tháng 8 Quận 10 TP HCM"
 
-def get_route():
+url = "https://maps.googleapis.com/maps/api/directions/json"
 
-    url = "https://maps.googleapis.com/maps/api/directions/json"
+params = {
+    "origin": origin,
+    "destination": destination,
+    "departure_time": "now",
+    "key": GOOGLE_API_KEY
+}
 
-    params = {
-        "origin": ORIGIN,
-        "destination": DESTINATION,
-        "departure_time": "now",
-        "traffic_model": "best_guess",
-        "mode": "driving",
-        "key": GOOGLE_API_KEY
-    }
+res = requests.get(url, params=params).json()
 
-    res = requests.get(url, params=params).json()
+if "routes" not in res or len(res["routes"]) == 0:
+    print("Google Maps API không trả dữ liệu")
+    exit(1)
 
-    leg = res["routes"][0]["legs"][0]
+leg = res["routes"][0]["legs"][0]
 
-    duration = leg["duration_in_traffic"]["text"]
+duration = leg["duration"]["text"]
 
-    link = "https://www.google.com/maps/dir/?api=1&origin=" + urllib.parse.quote(ORIGIN) + "&destination=" + urllib.parse.quote(DESTINATION)
+link = f"https://www.google.com/maps/dir/?api=1&origin={urllib.parse.quote(origin)}&destination={urllib.parse.quote(destination)}"
 
-    today = datetime.date.today().strftime("%d/%m/%Y")
+today = datetime.date.today()
 
-    msg = f"""
-🚦 LỘ TRÌNH ĐI LÀM {today}
+msg = f"""
+🚦 Lộ trình đi làm {today}
 
 Xuất phát
 Trường Võ Thị Sáu Q7
@@ -49,26 +49,19 @@ Google Maps
 {link}
 """
 
-    return msg
+zalo_url = "https://openapi.zalo.me/v2.0/oa/message"
 
-def send_zalo(msg):
+headers = {
+    "access_token": ZALO_ACCESS_TOKEN
+}
 
-    url = "https://openapi.zalo.me/v2.0/oa/message"
-
-    headers = {
-        "access_token": ZALO_ACCESS_TOKEN
+data = {
+    "recipient": {
+        "user_id": ZALO_USER_ID
+    },
+    "message": {
+        "text": msg
     }
+}
 
-    data = {
-        "recipient": {
-            "user_id": ZALO_USER_ID
-        },
-        "message": {
-            "text": msg
-        }
-    }
-
-    requests.post(url, headers=headers, json=data)
-
-msg = get_route()
-send_zalo(msg)
+requests.post(zalo_url, headers=headers, json=data)
